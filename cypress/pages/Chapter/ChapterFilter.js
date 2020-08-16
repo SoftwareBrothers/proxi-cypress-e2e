79217282eb51973f8e698ef53a2935fcd0303963
Reactor94 <v.pylypets94@gmail.com>
2020-08-16 18:31:42 +0200

    add Chapters selectors

---
+++ cypress/pages/Chapter/ChapterFilter.js
@@ -0,0 +1,10 @@
+export default class ChapterFilter {
+
+    static idField = '[name="filter-id"]';
+
+    static orderField = '[name="filter-order"]';
+
+    static isPremiumDropdown = '[data-testid="property-filter-isPremium"]';
+
+    static courseIdDropdown ='[data-testid="property-filter-courseId"]';
+}
