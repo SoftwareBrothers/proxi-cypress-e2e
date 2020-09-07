c6fd01e7f516722ed6d4fc3b5a7b275604c3b726
Reactor94 <v.pylypets94@gmail.com>
2020-09-07 15:55:38 +0200

    add filter actions and pages

--- cypress/pages/common/FilterActionsPO.js
+++ cypress/pages/common/FilterActionsPO.js
@@ -1,6 +1,8 @@
 export default class FilterActions {
 
-   static applyButton = 'form > section + section button'; //TODO: Add data-test-id
+   static closeOpenFilter = 'form h3 button';
+
+   static applyButton = 'form > section + section button:nth-child(1)'; //TODO: Add data-test-id
 
    static resetButton = 'form > section + section button:nth-child(2)'; //TODO: Add data-test-id
 }
